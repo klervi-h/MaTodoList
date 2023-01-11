41958a80ad4180d89617c55ed488b4e4774c2996
Klervi HÉNIQUE <klervi.henique@pole-emploi.fr>
2023-01-11 10:15:59 +0100

    Création du projet et changement de la première page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeComponent } from './liste/liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeComponent
   ],
   imports: [
     BrowserModule,
